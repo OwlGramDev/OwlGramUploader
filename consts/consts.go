cd5b48c8f55af4fcf2ec279abebf4b7a3f0ffff0
Laky64 <iraci.matteo@gmail.com>
2023-01-15 15:48:19 +0100

    Support to Android Studio Electric Eel

--- consts/consts.go
+++ consts/consts.go
@@ -3,4 +3,4 @@ package consts
 const ProjectPath = "C:/Users/iraci/StudioProjects/OwlGram"
 const BuildOutputsPath = ProjectPath + "/TMessagesProj_App/build/outputs"
 const ApiNotifyUrl = "https://app.owlgram.org/notify_upload"
-const JavaPath = "C:/Program Files/Android/Android Studio/jre"
+const JavaPath = "C:/Program Files/Android/Android Studio/jbr"
